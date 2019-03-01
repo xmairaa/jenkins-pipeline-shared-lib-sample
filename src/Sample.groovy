7930ba6d50751c40a0cf0f080c639711cc3baf01
Raja Maragani <raja.maragani@tcs.com>
2019-03-01 16:24:35 +0100

    Update Sample.groovy

--- src/Sample.groovy
+++ src/Sample.groovy
@@ -1,3 +1,6 @@
+import com.ericsson.eiffel.fem.config.EiffelJenkinsGlobalConfiguration
+import com.ericsson.eiffel.common.rest.ERClient
+
 public class Sample {
     private int x;
     private int y;
@@ -13,5 +16,9 @@ public class Sample {
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+    public String toErClient() {
+        final String erUrl = EiffelJenkinsGlobalConfiguration.get().getErRestUrl()
+        return ERClient.getInstance(erUrl)
     }
 }
